fd6df54a7912de2fd04d5918e2de37e06a8a09f8
Phil Fernandez <norcal618@gmail.com>
2021-03-19 03:26:32 -0700

    test_auth

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from project.server.auth.views import auth_blueprint
 
 app = Flask(__name__)
 
@@ -16,3 +17,5 @@ app.config.from_object(app_settings)
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+
+app.register_blueprint(auth_blueprint)
--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -1,1 +1,8 @@
 # project/server/auth/views.py
+
+from flask import Blueprint, request, make_response, jsonify
+from flask.views import MethodView
+from project.server import bcrypt, db
+from project.server.models import User
+
+auth_blueprint = Blueprint('auth', __name__)
