96e13f7d5c5c02ce2c67839953c68168fe84804b
Phil Fernandez <norcal618@gmail.com>
2021-03-18 21:20:48 -0700

    encode_auth_token in User model

--- project/server/models.py
+++ project/server/models.py
@@ -1,4 +1,5 @@
 import datetime
+import jwt
 from project.server import app, db, bcrypt
 
 
@@ -18,3 +19,22 @@ class User(db.Model):
         ).decode()
         self.registerd_on = datetime.datetime.now()
         self.admin = admin
+
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                app.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
